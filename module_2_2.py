73efee276bef3c687ebbe2b251a9c86ddf2539bc
vy541 <uumowd1@icloud.com>
2024-10-12 17:07:57 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+third = input('введите первое число:')
+first = input('введите второе число:')
+second = input('введите третье число:')
+if first == second and third == third:
+    print(3)
+elif second == first or second == third:
+    print(2)
+else:
+    print(0)
