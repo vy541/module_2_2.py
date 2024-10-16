e00528241c623b8591d90e7a79c6f4d702524b03
vy541 <uumowd1@icloud.com>
2024-10-16 15:51:17 +0300

    Add files via upload

--- module_2_2.py
+++ module_2_2.py
@@ -1,7 +1,7 @@
 third = input('введите первое число:')
 first = input('введите второе число:')
 second = input('введите третье число:')
-if first == second and third == third:
+if first == second and third == second:
     print(3)
 elif second == first or second == third:
     print(2)
